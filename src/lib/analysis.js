d80b72d18c58e7d19a712f3ae55ce1f03d7522c5
Alex Afonin <tuch@Afonin-AV-MBP.local>
2019-07-31 19:37:00 +0200

    eslint-fix analysis

--- src/lib/analysis.js
+++ src/lib/analysis.js
@@ -1,24 +1,29 @@
 import _ from 'lodash';
 
-export function hirschVolumes(orderBook, trimSize, blockSize) {
-    let trimmed = orderBook.trim(trimSize)
-    let bidH = _hirsch(0, 0, blockSize, trimmed.bids);
-    let offerH = _hirsch(0, 0, blockSize, trimmed.offers);
-    return [bidH, offerH];
+function _hirsch(current, max, blockSize, line) {
+  if (line.length <= current) {
+    return max;
+  }
+  const testSize = (current + 1) * blockSize;
+
+  const firstN = _.take(line, current + 1);
+
+  if (firstN.every(e => e.amount >= testSize)) {
+    const newMax = Math.max(max, current + 1);
+
+    return _hirsch(current + 1, newMax, blockSize, line);
+  }
+  const rest = _.drop(line, current);
+
+  return _hirsch(0, max, blockSize, rest);
 }
 
-function _hirsch(current, max, blockSize, line) {
-    if (line.length <= current) {
-        return max;
-    } else {
-        let testSize = (current + 1) * blockSize;
-        let firstN = _.take(line, current + 1);
-        if (firstN.every((e) => e.amount >= testSize)) {
-            let newMax = Math.max(max, current + 1);
-            return _hirsch(current + 1, newMax, blockSize, line);
-        } else {
-            let rest = _.drop(line, current);
-            return _hirsch(0, max, blockSize, rest);
-        }
-    }
+export function hirschVolumes(orderBook, trimSize, blockSize) {
+  const trimmed = orderBook.trim(trimSize);
+
+  const bidH = _hirsch(0, 0, blockSize, trimmed.bids);
+
+  const offerH = _hirsch(0, 0, blockSize, trimmed.offers);
+
+  return [bidH, offerH];
 }
